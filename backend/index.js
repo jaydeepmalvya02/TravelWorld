0e0afbc0b50afb2534cea9653221d00680b04269
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-01-24 14:05:45 +0530

    correct path setting

--- backend/index.js
+++ backend/index.js
@@ -49,7 +49,7 @@ app.listen(port, () => {
    connect()
    console.log('server listening on port', port)
 })
-app.use(express.static(path.join(__dirname, '/frontend/dist')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+   res.sendFile(path.join(__dirname, 'frontend', 'build'));
  })
