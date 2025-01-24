91b9e9f61a54260ce47b39c4d93173fb0f96c15f
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-01-24 13:12:38 +0530

    preinstallation

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import userRoute from './routes/users.js'
 import authRoute from './routes/auth.js'
 import reviewRoute from './routes/reviews.js'
 import bookingRoute from './routes/bookings.js'
+import path  from 'path';
+
 
 dotenv.config()
 const app = express()
@@ -40,7 +42,13 @@ app.use("/api/v1/users", userRoute)
 app.use("/api/v1/review", reviewRoute)
 app.use("/api/v1/booking", bookingRoute)
 
+
+
 app.listen(port, () => {
    connect()
    console.log('server listening on port', port)
 })
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+ })
