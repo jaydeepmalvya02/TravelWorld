d3ebf003af550d66ab3f704b52b4e171bf18163b
jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-02-15 18:18:51 +0530

    backend url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://travelworld-1-4qjy.onrender.com/api/v1'
-// export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+// const BASE_URL = 'https://travelworld-1-4qjy.onrender.com/api/v1'
+export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
