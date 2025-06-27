2f2fb054d3c5eb8bd893b0cd0f163288aed4c983
Jaydeepmalvya02 <jaydeepmalvya02@gmail.com>
2025-06-27 12:25:54 +0530

    fix typo error in login

--- backend/index.js
+++ backend/index.js
@@ -1,53 +1,57 @@
-import express  from "express";
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
 import cookieParser from "cookie-parser";
-import tourRoute from './routes/tours.js'
-import userRoute from './routes/users.js'
-import authRoute from './routes/auth.js'
-import reviewRoute from './routes/reviews.js'
-import bookingRoute from './routes/bookings.js'
+import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import reviewRoute from "./routes/reviews.js";
+import bookingRoute from "./routes/bookings.js";
 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
 
+const corsOptions = {
+  origin: [
+    "http://localhost:3000",
+    "https://travel-world-frontend-three.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
 
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 8000
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cookieParser());
 
+mongoose.set("strictQuery", false);
+const connect = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1); // Exit the process on failure
+  }
+};
 
-
-
-mongoose.set("strictQuery", false)
-const connect = async() => {
-   try {
-     const conn = await mongoose.connect(process.env.MONGO_URI, {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-     });
-
-     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-   } catch (error) {
-     console.error("❌ MongoDB connection failed:", error.message);
-     process.exit(1); // Exit the process on failure
-   }
-}
-
-app.use(express.json())
-app.use(cors())
-app.use(cookieParser())
-app.use("/api/v1/auth", authRoute)
-app.use("/api/v1/tours", tourRoute)
-app.use("/api/v1/users", userRoute)
-app.use("/api/v1/review", reviewRoute)
-app.use("/api/v1/booking", bookingRoute)
-app.get('/',async(req,res)=>{
-   res.json({message:"Thi is API "})
-})
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/review", reviewRoute);
+app.use("/api/v1/booking", bookingRoute);
+app.get("/", async (req, res) => {
+  res.json({ message: "Thi is API " });
+});
 
 connect();
 app.listen(port, () => {
-
-   console.log(`server listening on http://localhost:${port}`);
-})
+  console.log(`server listening on http://localhost:${port}`);
+});
